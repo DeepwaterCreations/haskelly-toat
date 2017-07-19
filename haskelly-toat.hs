77417c6ab4225e2dcacf3e0c9b6daa75b93edaa1
DeepwaterCreations <dylan.craine@deepwatercreations.com>
2017-07-18 22:51:57 -0600

    Make runnable from the command line

--- haskelly-toat.hs
+++ haskelly-toat.hs
@@ -1,4 +1,23 @@
 import System.Random
+import System.Environment
+
+main = do
+    args <- getArgs
+    handleArgs args
+
+handleArgs :: [String] -> IO ()
+handleArgs [] = showHelp
+handleArgs args = do
+    gen <- getStdGen
+    let vowelList = parseInput $ args !! 0
+        newWord = getRandomWord vowelList gen
+    putStrLn newWord
+
+parseInput :: String -> [Bool]
+parseInput(x:[]) = [(read [x]) == 0]
+parseInput (x:xs) = b:parseInput xs
+    where b = (read [x]) == 0
+
 
 getRandomLetter :: RandomGen g => Bool -> g -> (Char, g)
 getRandomLetter isVowel gen  = (letterSet !! idx, newGen)
@@ -10,3 +29,18 @@ getRandomWord :: RandomGen g => [Bool] -> g -> String
 getRandomWord (v:[]) gen = [fst $ getRandomLetter v gen]
 getRandomWord (v:vs) gen = randomLetter:(getRandomWord vs newGen)
     where (randomLetter, newGen) = getRandomLetter v gen
+
+showHelp = do
+    putStrLn ""
+    putStrLn "Usage: Takes a string of 1s and 0s, returns a brand new word."
+    putStrLn "0s are vowels. 1s are consonants."
+    putStrLn ""
+    putStrLn "Example:" 
+    putStrLn "$ haskelly-toat \"10101010\""
+    -- putStrLn "zupuceli"
+    gen <- getStdGen
+    let vowelList = parseInput "10101010"
+        newWord = getRandomWord vowelList gen
+    putStrLn newWord
+    putStrLn ""
+
